39c2dde37e4a76bdeb056541f1ef03938acb72de
Gertin Pakkonen <gertinnn@tlu.ee>
2019-04-03 13:56:19 +0300

    Lisatud esimene töö

---
+++ Work1/Program.java
@@ -0,0 +1,8 @@
+
+public class Program{
+    public static void main(String[] args) {
+        FileReading jaagupi_text = new FileReading("/Users/gertinpakkonen/Desktop/Git/Programmeerimise-pohikursus-2019/Work1/andmed.txt");
+        System.out.println(jaagupi_text.word_counter);
+
+    }
+}
