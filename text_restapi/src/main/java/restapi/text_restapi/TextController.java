fa2665dda213a6de52c02ac21cf98f8719ed0485
Gertin Pakkonen <gertinnn@tlu.ee>
2019-04-10 14:03:43 +0300

    Täiendatud ja tehtud uus versioon REST API ülesandest

--- text_restapi/src/main/java/restapi/text_restapi/TextController.java
+++ text_restapi/src/main/java/restapi/text_restapi/TextController.java
@@ -1,16 +1,17 @@
 package restapi.text_restapi;
 
+import java.util.HashMap;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 @RestController
 public class TextController {
 
     @PostMapping("/text")
-    public Text addText(@RequestBody Map<String, String> body) {
+    public Text addText(@RequestBody HashMap<String, String> body) {
         String input = body.get("text");
         Text newText = new Text(input);
         return newText;
